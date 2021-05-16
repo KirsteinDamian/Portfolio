d911ed678301440298757702899dadcf28268414
Damian Kirstein <kirsteindamian@gmail.com>
2021-05-16 16:16:30 +0200

    home page intro

--- portfolio_project/portfolio/urls.py
+++ portfolio_project/portfolio/urls.py
@@ -17,7 +17,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+import jobs.views as job_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', job_views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
