c5b1e34fba4569da924f1621d691f797a2f0dbb3
Damian Kirstein <kirsteindamian@gmail.com>
2021-05-17 14:21:19 +0200

    All blogs

--- portfolio_project/portfolio/urls.py
+++ portfolio_project/portfolio/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-import jobs.views as job_views
+from django.urls import path, include
+import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', job_views.home, name='home'),
+    path('', jobs.views.home, name='home'),
+    path('blog/', include('blog.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
